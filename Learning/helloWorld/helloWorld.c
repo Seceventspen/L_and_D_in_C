409112bab9e1ea13b4c67a1cd6d2bc03585df1ff
Seceventspen <seceventspen@alternativesec.xyz>
2024-05-10 10:43:52 +0100

    Update helloWorld.c

--- Learning/helloWorld/helloWorld.c
+++ Learning/helloWorld/helloWorld.c
@@ -1,1 +1,11 @@
+// Hello World in C
+// by a C noob ^_^ Seceventspen
+// helloWorld.c
 
+#include <stdio.h>
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World 2024!");
+   return 0;
+}
